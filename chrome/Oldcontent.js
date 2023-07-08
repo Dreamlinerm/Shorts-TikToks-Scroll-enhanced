80f0bb118dd2f8f7ede40d14798d088f4948761a
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2023-07-08 14:25:07 +0200

    1.0.7

--- chrome/Oldcontent.js
+++ chrome/Oldcontent.js
@@ -12,7 +12,7 @@
  */
 let url = window.location.href;
 const isShort = /shorts/i.test(url);
-const version = "1.0.6";
+const version = "1.0.7";
 // global variables in localStorage
 const defaultSettings = {
   settings: {
